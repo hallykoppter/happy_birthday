109929374f0ee3bb7bd33f6bafbac2f5fd47a1da
hallykoppter <officialkurniasandi@gmail.com>
2024-09-10 19:59:21 +0700

    typewriter

---
+++ src/app/Components/Header/index.js
@@ -0,0 +1,21 @@
+"use client"
+import { Typewriter } from "react-simple-typewriter"
+
+const Header = () => {
+  const word = [
+    "Selamat ulang tahun, Sayang",
+    "Semoga sehat selalu",
+    "Semoga semua yang diharapkan",
+    "bisa terkabul",
+  ]
+
+  return (
+    <div className="flex justify-center align-middle min-w-[100vw] bg-primary p-3">
+      <div className="text-white font-titan">
+        - <Typewriter words={word} typeSpeed={100} loop={false} /> -
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        baloo: '"Baloo Bhai 2", sans-serif',
+        titan: '"Titan One", sans-serif',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        body: "#fdf4f5",
+        primary: "#4299E1",
+        secondary: "#FF6347",
+        secondaryH: "#FF5865",
+        third: "#BC94C8",
       },
     },
   },
   plugins: [],
-};
+}
